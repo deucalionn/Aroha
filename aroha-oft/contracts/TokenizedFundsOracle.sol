d336ba00c08382b7ec56278e9b5060a140f42d0f
Magicred-1 <djason.gadiou.sio@gmail.com>
2024-11-16 01:55:08 +0700

    Pyth feeds

---
+++ aroha-oft/contracts/TokenizedFundsOracle.sol
@@ -0,0 +1,66 @@
+pragma solidity ^0.8.0;
+ 
+import "@pythnetwork/pyth-sdk-solidity/IPyth.sol";
+import "@pythnetwork/pyth-sdk-solidity/PythStructs.sol";
+ 
+contract TokenizedFundsOracle {
+    IPyth pyth;
+    
+    /**
+    * @param pythContract The address of the Pyth contract
+    */
+    constructor(address pythContract) {
+        // The IPyth interface from pyth-sdk-solidity provides the methods to interact with the Pyth contract.
+        // Instantiate it with the Pyth contract address from https://docs.pyth.network/price-feeds/contract-addresses/evm
+        pyth = IPyth(pythContract);
+    }
+
+    function convertToUint(
+        PythStructs.Price memory price,
+        uint8 targetDecimals
+    ) private pure returns (uint256) {
+        if (price.price < 0 || price.expo > 0 || price.expo < -255) {
+            revert InvalidPriceExponent();
+        }
+
+        uint8 priceDecimals = uint8(uint32(-1 * price.expo));
+
+        if (targetDecimals >= priceDecimals) {
+            return
+                uint(uint64(price.price)) *
+                10 ** uint32(targetDecimals - priceDecimals);
+        } else {
+            return
+                uint(uint64(price.price)) /
+                10 ** uint32(priceDecimals - targetDecimals);
+        }
+    }
+ 
+    /**
+        * This method is an example of how to interact with the Pyth contract.
+        * Fetch the priceUpdate from Hermes and pass it to the Pyth contract to update the prices.
+        * Add the priceUpdate argument to any method on your contract that needs to read the Pyth price.
+        * See https://docs.pyth.network/price-feeds/fetch-price-updates for more information on how to fetch the priceUpdate.
+    
+        * @param priceUpdate The encoded data to update the contract with the latest price
+        */
+    function fetchPriceFeed(bytes[] calldata _priceUpdate, address _priceFeedAddress, uint8 _decimals) public payable returns (uint256) {
+        uint256 
+
+        // Submit a priceUpdate to the Pyth contract to update the on-chain price.
+        // Updating the price requires paying the fee returned by getUpdateFee.
+        // WARNING: These lines are required to ensure the getPriceNoOlderThan call below succeeds. If you remove them, transactions may fail with "0x19abf40e" error.
+        uint fee = pyth.getUpdateFee(_priceUpdate);
+        pyth.updatePriceFeeds{ value: fee }(_priceUpdate);
+    
+        // Read the current price from a price feed if it is less than 60 seconds old.
+        // Each price feed (e.g., ETH/USD) is identified by a price feed ID.
+        // The complete list of feed IDs is available at https://pyth.network/developers/price-feed-ids
+        bytes32 priceFeedId = _priceFeedAddress;
+        PythStructs.Price memory price = pyth.getPriceNoOlderThan(priceFeedId, 60);
+
+        // Convert the price to the desired number of decimals.
+        return uint256 priceInWei = convertToUint(price, _decimals);
+    }
+}
+ 
