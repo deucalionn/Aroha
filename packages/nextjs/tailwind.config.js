1d94b058c22e9da53808ad151f5556063c8aef41
Sakshi Shah <sakshi.shah299@gmail.com>
2024-11-16 02:06:19 +0700

    fix: Invest page layout added

--- packages/nextjs/tailwind.config.js
+++ packages/nextjs/tailwind.config.js
@@ -80,6 +80,9 @@ module.exports = {
       boxShadow: {
         center: "0 0 12px -2px rgb(0 0 0 / 0.05)",
       },
+      colors: {
+        wheat: "rgb(241, 192, 134)",
+      },
       animation: {
         "pulse-fast": "pulse 1s cubic-bezier(0.4, 0, 0.6, 1) infinite",
       },
