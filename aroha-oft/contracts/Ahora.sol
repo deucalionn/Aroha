cfd75eed4ea2e653d65bfcbba126627ee3c582d8
Magicred-1 <djason.gadiou.sio@gmail.com>
2024-11-16 00:27:59 +0700

    Merkle Tree

--- aroha-oft/contracts/Ahora.sol
+++ aroha-oft/contracts/Ahora.sol
@@ -1,13 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.22;
 
-import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract Ahora is Ownable {
     bytes32 public merkleRoot;
 
     mapping(address => bool) private isWhitelisted;
+
+    constructor(address _initialOwner, bytes32 _merkleRoot)
+        Ownable(_initialOwner)
+    {
+        merkleRoot = _merkleRoot;
+    }
 
     function setMerkleRoot(bytes32 _merkleRoot) external onlyOwner {
         merkleRoot = _merkleRoot;
@@ -18,8 +24,7 @@ contract Ahora is Ownable {
     }
 
     function isWhitelisted(address _account, bytes32[] calldata _proof) internal view returns(bool) {
-        bytes32 leaf = keccak256(abi.encode(keccak256(abi.encode(_account))));
-        return MerkleProof.verify(_proof, merkleRoot, leaf);
+        bytes32 leaf = keccak256(abi.encodePacked(_account));
+        return MerkleProof.verify(merkleRoot, leaf, _proof);
     }
-
 }
