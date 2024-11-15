f50e80b68d026942acd5b930ea3d48cfa2d2993a
Magicred-1 <djason.gadiou.sio@gmail.com>
2024-11-15 22:26:18 +0700

    First commit

---
+++ src/lib/tokens.utils..ts
@@ -0,0 +1,14 @@
+import { hederaTestnet, lineaSepolia, rootstockTestnet, unichainSepolia, zircuitTestnet } from "viem/chains";
+
+export const stablecoins = {
+    [zircuitTestnet.id]: {
+    },
+    [unichainSepolia.id]: {
+    },
+    [rootstockTestnet.id]: {
+    },
+    [lineaSepolia.id]: {
+    },
+    [hederaTestnet.id]: {
+    },
+}
