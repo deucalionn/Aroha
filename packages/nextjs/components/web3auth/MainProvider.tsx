cea55a80ed0d8fb5214d15cc302f7da8b29940d1
Sakshi Shah <sakshi.shah299@gmail.com>
2024-11-15 21:04:27 +0700

    feat: web3auth modal

---
+++ packages/nextjs/components/web3auth/MainProvider.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { web3AuthContextConfig } from "./provider/config";
+import { Web3AuthProvider } from "@web3auth/no-modal-react-hooks";
+import { XRPLChainProvider } from "./XRPLProvider/XRPLWrapper";
+import { Toaster } from "sonner";
+
+export default function MainProvider({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <Web3AuthProvider config={web3AuthContextConfig}>
+      <Toaster />
+        <XRPLChainProvider>
+          {children}
+        </XRPLChainProvider>
+    </Web3AuthProvider>
+  );
+}
