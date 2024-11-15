7e618096f99fbaa291740a9dd10f4e2b17a7c77d
Lucas Pereira <lucas.pereira@intercellar.io>
2024-11-16 03:23:09 +0700

    remove useless code

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -2,9 +2,6 @@ import { Footer } from "@/components/Footer";
 import { SidebarComponent } from "@/components/SidebarComponent";
 import { Providers } from "@/components/web3auth/provider/providers";
 import "@/styles/globals.css";
-import { getMetadata } from "@/utils/scaffold-eth/getMetadata";
-
-export const metadata = getMetadata({ title: "Scaffold-ETH 2 App", description: "Built with 🏗 Scaffold-ETH 2" });
 
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -13,7 +10,6 @@ const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
         <Providers>
           <div className="flex h-screen">
             <SidebarComponent />
-
             <div className="flex-1 p-6 bg-gradient-to-l from-gray-900 to-black">{children}</div>
           </div>
           <Footer />
